cd2471b21c61f547dc0b5c0d09416fdec6a78ecf
Luz-Helena <107291710+Luz-Helena@users.noreply.github.com>
2025-08-27 11:22:57 -0300

    Fazer

---
+++ Aula08/exercicio3.py
@@ -0,0 +1,18 @@
+'''
+Exercicio 3: Codificador e Decodificador de Frases
+Crie um programa que codifica e decotifica uma frase, seguindo as regras abaixo:
+Cada vogal deve ser substituida pelo numero correspondente:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+
+O programa deve:
+
+1. Ler uma frase digitada pelo usuario.
+2. Exibir a frase codificada, trocando as vogais pelos numeros.
+3. Exibir a frase decodificada, voltando os numeros as vogais originais.
+
+'''
