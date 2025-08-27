03e9cedbdc1ef408d5e7a314e50f902defa6cfee
Luz-Helena <107291710+Luz-Helena@users.noreply.github.com>
2025-08-27 11:44:26 -0300

    Pratic4

---
+++ Aqui/idade.py
@@ -0,0 +1,12 @@
+idade = int(input("Digite sua idade:"))
+
+if 0 <= idade <=12:
+    print("Você é criança")
+elif 13 <= idade <= 17:
+    print("Você é adolescente")
+elif  18 <= idade <= 59:
+    print("Você é adulto")
+elif 60<= idade <=100:
+    print("Você é idoso")
+else: 
+    print("MUMIA DO KRL")
