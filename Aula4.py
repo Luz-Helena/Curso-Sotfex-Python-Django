cd32b523a962ed0cbd2ed76ad08787ef11d0c5d6
Luz-Helena <ritter.alex@hotmail.com>
2025-09-10 11:13:01 -0300

    Aula4

---
+++ Aula4.py
@@ -0,0 +1,12 @@
+'''Como Ativar a Receita (Chamando a Função)'''
+def diga_oi():
+    print('Oi! Tudo bem?')
+diga_oi()
+
+'''Parâmetros: Os Ingredientes da Função'''
+def saudar(nome):
+    print(f'Olá,{nome}!')
+saudar('Maria')
+saudar('João')
+
+'''O Poder do return'''
---
+++ teste.py
@@ -0,0 +1,9 @@
+def dados() -> dict:
+    return{
+        "atendente": 'Maria',
+        "paes": {
+            "frances":{'nome': 'Pão de frances', 'valor': 0.50, 'quantidade':15},
+            "doce":{'nome': 'Pão Doce', 'valor': 5.00,'quantidade':20},
+            
+        }
+    }
