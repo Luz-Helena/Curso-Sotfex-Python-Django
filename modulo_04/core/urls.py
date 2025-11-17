4218e4a212dd3c8378b7cce5c446e5877026ba8c
Luz-Helena <107291710+Luz-Helena@users.noreply.github.com>
2025-11-17 11:06:07 -0300

    apostila5

--- modulo_04/core/urls.py
+++ modulo_04/core/urls.py
@@ -3,5 +3,7 @@ from . import views # O '.' importa as 'views' do app atual
 urlpatterns = [ 
     # Quando a URL for a raiz (''), chame a função 'home' de 'views.py' 
     path('', views.home, name='home'), 
-    path('home2/', views.home, name='home'), 
+    path('home2/', views.home, name='home'),
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
 ]
--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404 
 from django.http import HttpResponse 
 
 # Create your views here.
@@ -10,3 +10,16 @@ def home(request):
 def home2(request): 
     # Vamos retornar a resposta HTTP mais simples: um texto HTML 
     return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+
+def concluir_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST': 
+        tarefa.concluida = True 
+        tarefa.save() 
+    return redirect('home')
+
+def deletar_tarefa(request, pk): 
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST': 
+        tarefa.delete() 
+    return redirect('home') 
