a95fed2587c92b1092bc349f61efd114d5d8e375
Luz-Helena <107291710+Luz-Helena@users.noreply.github.com>
2025-11-29 00:15:20 -0300

    Aula_02

---
+++ modulo_01/aula_02/02_exercicio.py
@@ -0,0 +1,10 @@
+preco = float(input("Digite o preço do produto: "))
+if preco > 100:
+    desconto = preco * 0.10
+    novo_preco = preco - desconto
+    print(f"O novo preço com 10% de desconto é R$ {novo_preco:.2f}.")
+else:
+    print("Preço sem desconto.")
+    numero = int(input("Digite um número: "))
+for i in range(1, 11):
+    print(f"{numero} x {i} = {numero * i}")
---
+++ modulo_01/aula_02/04_exercicio.py
@@ -0,0 +1,8 @@
+senha_secreta = "python123"
+senha_digitada = input("Digite a senha: ")
+
+if senha_digitada == senha_secreta:
+    print("Acesso concedido.")
+else:
+    print("Senha incorreta.")
+    
